060d6e2f7aabdc69e071e423ae0ac41a74464659
Kogamishinya1995 <79081103268@yandex.ru>
2024-11-24 16:16:37 +0300

    3 commit

--- src/components/Main/Transfers/Transfers.tsx
+++ src/components/Main/Transfers/Transfers.tsx
@@ -1,3 +1,65 @@
-const Transfers = () => <div className="TransferBar">Transfers</div>;
+/* eslint-disable @cspell/spellchecker */
+import { Formik, Form, Field } from "formik";
+
+const Transfers = () => (
+  <div className="transferBar">
+    <h2>Количество пересадок</h2>
+    <Formik
+      initialValues={{
+        transfer1: false,
+        transfer2: false,
+        transfer3: false,
+        transfer4: false,
+        transfer5: false,
+      }}
+      onSubmit={() => {}}
+    >
+      {({ values, setFieldValue }) => (
+        <Form>
+          <label htmlFor="transfer1">
+            <Field
+              type="checkbox"
+              name="transfer1"
+              onChange={() => setFieldValue("transfer1", !values.transfer1)}
+            />
+            Все
+          </label>
+          <label htmlFor="transfer2">
+            <Field
+              type="checkbox"
+              name="transfer2"
+              onChange={() => setFieldValue("transfer2", !values.transfer2)}
+            />
+            Без пересадок
+          </label>
+          <label htmlFor="transfer3">
+            <Field
+              type="checkbox"
+              name="transfer3"
+              onChange={() => setFieldValue("transfer3", !values.transfer3)}
+            />
+            1 пересадка
+          </label>
+          <label htmlFor="transfer4">
+            <Field
+              type="checkbox"
+              name="transfer4"
+              onChange={() => setFieldValue("transfer4", !values.transfer4)}
+            />
+            2 пересадки
+          </label>
+          <label htmlFor="transfer5">
+            <Field
+              type="checkbox"
+              name="transfer5"
+              onChange={() => setFieldValue("transfer5", !values.transfer5)}
+            />
+            3 пересадки
+          </label>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default Transfers;
